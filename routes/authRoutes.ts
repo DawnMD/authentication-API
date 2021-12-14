52eb9b0c98d772a441c89262efd84b3b4e30b119
Mainak Das <mainakdas104@gmail.com>
2021-12-15 00:04:39 +0530

    Revert "Add lint deps"
    This reverts commit 16bd63c68317a046e0fe7d4b9659943c305c6332.

--- routes/authRoutes.ts
+++
@@ -1,9 +0,0 @@
-import { Request, Response, Router } from 'express';
-
-const router = Router();
-
-router.get('/register', (req: Request, res: Response) => {
-  const username = req.body.username;
-});
-
-export default router;
