8cf2aaf7cc768e8967414156636d041b5ae40108
Mainak Das <mainakdas104@gmail.com>
2021-12-16 23:42:51 +0530

    Add compare password function

--- utils/passwordUtils.ts
+++ utils/passwordUtils.ts
@@ -4,3 +4,10 @@ export const hashPassword = async (passowrd: string) => {
   const hashed = await argon2.hash(passowrd);
   return hashed;
 };
+
+export const comparePassowrd = async (
+  hashedPassword: string,
+  password: string
+) => {
+  return await argon2.verify(hashedPassword, password);
+};
