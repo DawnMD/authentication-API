f83bda8ba3ccff25081712a7cca690aa6d92cbf7
Mainak Das <mainakdas104@gmail.com>
2021-12-16 23:43:34 +0530

    Add routes for register, login & delete account

--- routes/authRoutes.ts
+++ routes/authRoutes.ts
@@ -1,15 +1,10 @@
-import { Request, Response, Router } from 'express';
-import { hashPassword } from '../utils/passwordUtils';
+import { Router } from 'express';
+import { deleteAccount, login, register } from '../controllers/authController';
 
 const router = Router();
 
-router.post('/register', async (req: Request, res: Response) => {
-  const username = req.body.username;
-  const password = req.body.password;
-  const fname = req.body.fname;
-  const lname = req.body.lname;
-  const hashedPassword = await hashPassword(password);
-  res.status(201).json({ username, hashedPassword, fname, lname });
-});
+router.post('/register', register);
+router.post('/login', login);
+router.delete('/delete-account', deleteAccount);
 
 export default router;
