65c4ee2a41a33900db72a1103692cb5f00b17f68
Mainak Das <mainakdas104@gmail.com>
2021-12-20 23:18:20 +0530

    Add refresh token route

--- routes/authRoutes.ts
+++ routes/authRoutes.ts
@@ -1,10 +1,16 @@
 import { Router } from 'express';
-import { deleteAccount, login, register } from '../controllers/authController';
+import {
+  deleteAccount,
+  login,
+  refreshToken,
+  register,
+} from '../controllers/authController';
 
 const router = Router();
 
 router.post('/register', register);
 router.post('/login', login);
+router.post('/refresh-token', refreshToken);
 router.delete('/delete-account', deleteAccount);
 
 export default router;
